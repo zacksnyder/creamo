71564c4bfa95837b7e0fa4c2fda192e67bcd05a3
zacksnyder <you@example.com>
2018-12-21 16:42:09 +0900

    creamo

--- creamo.js
+++ creamo.js
@@ -11,18 +11,21 @@
     ext.setDigitalvalue = function() {
         // Code that gets executed when the block is run
     };
-
+    ext.setDigitalonoff = function() {
+        // Code that gets executed when the block is run
+    };
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
             [' ', 'set %m.digital value to %n', 'setDigitalvalue','Red LED',255],
-            ['r','creamo2', 'my_first_block']
+            [' ', 'set %m.digital value to %m.on', 'setDigitalonoff','Red LED','on']
         ],
         menus:{
             digital:["Speaker","Red LED","Green LED","Blue LED","Moter1 left","Moter1 right","Moter2 left","Moter2 right",
                      "Red FND","Green FND","Blue FND","Red Matrix","Green Matrix","Blue Matrix"
-        ]
+        ],
+        on:["on","off"]
         }
     };
 
